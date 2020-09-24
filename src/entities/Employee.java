26692fab5fe4d81c9689cf1c8ba81b9d112f559e
Yuri Baldan <yuribbp@gmail.com>
2020-09-24 12:58:08 -0300

    Order by salary

--- src/entities/Employee.java
+++ src/entities/Employee.java
@@ -29,6 +29,6 @@ public class Employee implements Comparable<Employee> {
 
 	@Override
 	public int compareTo(Employee other) {
-		return name.compareTo(other.getName());
+		return salary.compareTo(other.getSalary());
 	}
 }
